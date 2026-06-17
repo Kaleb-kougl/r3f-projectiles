b21e46f3a0cc229fef137c9a315f4fe02ee56554
Kaleb-kougl <kalebkougl@gmail.com>
2026-06-17 16:15:07 -0700

    feat: add postinstall script to display developer portfolio link

---
+++ postinstall.js
@@ -0,0 +1,13 @@
+const reset = "\x1b[0m";
+const bgBlue = "\x1b[44m";
+const white = "\x1b[37m";
+const cyan = "\x1b[36m";
+const dim = "\x1b[2m";
+
+console.log("");
+console.log(`${bgBlue}${white} r3f-projectiles ${reset}  Thank you for using this library.`);
+console.log(`${dim}-------------------------------------------------------------------${reset}`);
+console.log(`I am a solo developer and I'm currently looking for a new role.`);
+console.log(`If your engineering team is hiring, I would love to connect:`);
+console.log(`${cyan}https://kalebkougl-portfolio.vercel.app/${reset}`);
+console.log("");
